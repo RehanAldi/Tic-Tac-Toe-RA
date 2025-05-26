4c485d610d14d7443c5493eac298961d8a2d62dc
RehanAldi <rehannn12e@gmail.com>
2025-05-26 21:48:01 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,21 +6,9 @@ const urlsToCache = [
   "./script.js",
   "./manifest.json",
   "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+    caches.open
